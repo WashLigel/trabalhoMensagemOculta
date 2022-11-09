91abe5bcf42a905029aafd38f8575e0ff0690e69
WashLigel <113737159+WashLigel@users.noreply.github.com>
2022-11-08 21:12:52 -0300

    update supremo

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
 const cifraDC = document.getElementById('cifraDC');
-const base64 = document.getElementById('base64');
+const base64 = document.getElementById('base64'); 
 const box1 = document.getElementById('box1');
 const number2 = document.getElementById('number2');
 const codificador = document.getElementById('codificador');
@@ -30,12 +30,13 @@ function cifraBase(){
     if (base64.checked) {
         base();   
     }
-    else{
+    else if (cifraDC.checked){
         cifra();
 
     }
+    else{}
+    }
 
-}
 function base(){
     let mensagem = "";
     if (codificador.checked){
@@ -46,7 +47,33 @@ function base(){
     }
     box2.innerHTML = mensagem;
 }
+
 function cifra(){
+    const alfa = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const acentos = 'áéíóúâêîôûàèìòùã'.split('');
+    const maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const maiusculasAcentos = 'ÁÉÍÓÚÃÀÈÌÒÙÕ'.split('');
+    const respostaFinal = [];
+    const x = parseInt(incremento.value);
+
+    if (codificador.checked){
+        for (let i=0; i<box1.value.length;i++){
+            for ( let n=0; n<alfa.length;n++){
+                if (box1.value[i] == alfa[n]){
+                    respostaFinal[i] = alfa[(n+x)%alfa.length]
+                    break;
+                }
+            }
+        }
+        box2.value = respostaFinal.join('');
+    }
+        else if (decodificador.checked){
+
+    }
+    
+    else {}
+
+
 
 }
 
