dfbace89a97a2e24a38c3b68ad1e2a5e203b3616
WashLigel <113737159+WashLigel@users.noreply.github.com>
2022-11-12 20:18:02 -0300

    UPDATE ULTRA MEGA POWER SUPREMO

--- Js/script.js
+++ Js/script.js
@@ -1,54 +1,64 @@
-const cifraDC = document.getElementById('cifraDC');
-const base64 = document.getElementById('base64'); 
-const box1 = document.getElementById('box1');
-const number2 = document.getElementById('number2');
-const codificador = document.getElementById('codificador');
-const decodificar = document.getElementById('decodificar');
-const box2 = document.getElementById('box2');
-const enviar = document.getElementById('enviar');
-const incremento = document.getElementById('incremento');
+function trocaTexto() {
+    const codificador = document.getElementById('codificador');
+    const enviar = document.getElementById('enviar');
 
-
-function trocaTexto(){
-if(codificador.checked){
-    enviar.innerText = 'Codificar';
-}  else{
-    enviar.innerText = 'Decodificar';
-}
+    if (codificador.checked) {
+        enviar.innerText = 'Codificar';
+    }
+    else {
+        enviar.innerText = 'Decodificar';
+    }
 }
 
-function mostraIncremento(){
-    if (cifraDC.checked){
+function mostraIncremento() {
+    const cifraDC = document.getElementById('cifraDC');
+    const incremento = document.getElementById('incremento');
+
+    if (cifraDC.checked) {
         incremento.style.display = 'block';
     }
-    else{ incremento.style.display = 'none';
+    else {
+        incremento.style.display = 'none';
 
     }
 }
 
-function cifraBase(){
+function cifraBase() {
+    const cifraDC = document.getElementById('cifraDC');
+    const base64 = document.getElementById('base64');
+
     if (base64.checked) {
-        base();   
+        base();
     }
-    else if (cifraDC.checked){
+    else if (cifraDC.checked) {
         cifra();
-
     }
-    else{}
+    else {
+        alert("Escolha entre Base 64 ou Cifra de César.")
     }
+}
 
-function base(){
+function base() {
+    const codificador = document.getElementById('codificador');
+    const box1 = document.getElementById('box1');
+    const box2 = document.getElementById('box2');
+
     let mensagem = "";
-    if (codificador.checked){
-        mensagem = window.btoa(box1.value); 
+    if (codificador.checked) {
+        mensagem = window.btoa(box1.value);
     }
-    else{
+    else {
         mensagem = window.atob(box1.value);
     }
     box2.innerHTML = mensagem;
 }
 
-function cifra(){
+function cifra() {
+    const incremento = document.getElementById('number2');
+    const box1 = document.getElementById('box1');
+    const codificador = document.getElementById('codificador');
+    const decodificar = document.getElementById('decodificar');
+    const box2 = document.getElementById('box2');
     const alfa = 'abcdefghijklmnopqrstuvwxyz'.split('');
     const acentos = 'áéíóúâêîôûàèìòùã'.split('');
     const maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
@@ -56,25 +66,70 @@ function cifra(){
     const respostaFinal = [];
     const x = parseInt(incremento.value);
 
-    if (codificador.checked){
-        for (let i=0; i<box1.value.length;i++){
-            for ( let n=0; n<alfa.length;n++){
-                if (box1.value[i] == alfa[n]){
-                    respostaFinal[i] = alfa[(n+x)%alfa.length]
+    if (codificador.checked) {
+        for (let i = 0; i < box1.value.length; i++) {
+            for (let n = 0; n < alfa.length; n++) {
+                if (box1.value[i] == alfa[n]) {
+                    respostaFinal[i] = alfa[(n + x) % alfa.length];
                     break;
+                } else if (box1.value[i] == acentos[n]) {
+                    respostaFinal[i] = acentos[(n + x) % acentos.length];
+                    break;
+                } else if (box1.value[i] == maiusculas[n]) {
+                    respostaFinal[i] = maiusculas[(n + x) % maiusculas.length];
+                    break;
+                } else if (box1.value[i] == maiusculasAcentos[n]) {
+                    respostaFinal[i] = maiusculasAcentos[(n + x) % maiusculasAcentos.length];
+                    break;
+                } else {
+                    respostaFinal[i] = box1.value[i];
                 }
             }
         }
-        box2.value = respostaFinal.join('');
+        box2.innerHTML = respostaFinal.join('');
     }
-        else if (decodificador.checked){
-
+    else if (decodificar.checked) {
+        for (let i = 0; i < box1.value.length; i++) {
+            for (let n = 0; n < alfa.length; n++) {
+                if (box1.value[i] == alfa[n]) {
+                    let neg = n - x;
+                    for (let pos = neg; pos < 0; pos++) {
+                        neg += 26;
+                        pos = neg;
+                    }
+                    respostaFinal[i] = alfa[neg];
+                    break;
+                } else if (box1.value[i] == acentos[n]) {
+                    let neg = n - x;
+                    for (let pos = neg; pos < 0; pos++) {
+                        neg += 26;
+                        pos = neg;
+                    }
+                    respostaFinal[i] = alfa[neg];
+                    break;
+                } else if (box1.value[i] == maiusculas[n]) {
+                    let neg = n - x;
+                    for (let pos = neg; pos < 0; pos++) {
+                        neg += 26;
+                        pos = neg;
+                    }
+                    respostaFinal[i] = alfa[neg];
+                    break;
+                } else if (box1.value[i] == maiusculasAcentos[n]) {
+                    let neg = n - x;
+                    for (let pos = neg; pos < 0; pos++) {
+                        neg += 26;
+                        pos = neg;
+                    }
+                    respostaFinal[i] = alfa[neg];
+                    break;
+                } else {
+                    respostaFinal[i] = box1.value[i];
+                }
+            }
+        }
+        box2.innerHTML = respostaFinal.join('');
     }
-    
-    else {}
-
-
-
 }
 
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+Nesse trabalho eu tive de fazer a criptografia de base 64 e cifra de césar.
+
+Como codificar utilizando o Base64? O método Base64 leva esse nome por ser constituído de 64 caracteres como base de codificação. Esses caracteres podem ser ([A-Z], [a-z], [0-9], “+” e “/”). Além desses caracteres, o Base64 utiliza o carácter “=” como um sufixo especial dentro da codificação.
+
+
+Como codificar utilizando o Cifra De César?
+Basta escrever na caixa sua mensagem, escolher a quantidade de número que deseja icrementar e clicar em codificar.
+Cada letra do alfabeto era substituída pela letra três posições à frente, ou seja, o “A” era substituído pelo “D”, o “B” pelo “E”, o “C” pelo “F”, e assim sucessivamente. Qualquer código que tenha esse padrão é considerado uma Cifra de César, também conhecida como Código de César.
